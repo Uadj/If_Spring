c1bd8a4d5c39a7471defe785fc50f3e58f1f8035
DGW <ds257873@naver.com>
2023-10-23 19:02:06 +0900

    Spring으로 전환

--- 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/MemberApp.java
+++ 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/MemberApp.java
@@ -4,12 +4,18 @@ import com.example.base_shopping.member.Grade;
 import com.example.base_shopping.member.Member;
 import com.example.base_shopping.member.MemberService;
 import com.example.base_shopping.member.MemberServiceImpl;
+import com.example.base_shopping.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/OrderApp.java
+++ 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/OrderApp.java
@@ -7,19 +7,25 @@ import com.example.base_shopping.member.MemberServiceImpl;
 import com.example.base_shopping.order.Order;
 import com.example.base_shopping.order.OrderService;
 import com.example.base_shopping.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
         Order order = orderService.createOrder(1L, "itemA", 10000);
-        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
     }
 
 }
