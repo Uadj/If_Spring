8d90c9ae4466e485cf032305f886b838b01a1055
DGW <ds257873@naver.com>
2023-10-23 21:05:41 +0900

    Singleton

---
+++ 스프링 핵심원리 기본편/base_shopping/src/test/java/com/example/base_shopping/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,40 @@
+package com.example.base_shopping.singleton;
+
+import com.example.base_shopping.AppConfig;
+import com.example.base_shopping.member.MemberRepository;
+import com.example.base_shopping.member.MemberServiceImpl;
+import com.example.base_shopping.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println(memberRepository1);
+        System.out.println(memberRepository2);
+        System.out.println(memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
--- 스프링 핵심원리 기본편/base_shopping/src/test/java/com/example/base_shopping/singleton/SingletonService.java
+++ 스프링 핵심원리 기본편/base_shopping/src/test/java/com/example/base_shopping/singleton/SingletonService.java
@@ -2,25 +2,21 @@ package com.example.base_shopping.singleton;
 
 import com.example.base_shopping.AppConfig;
 import com.example.base_shopping.member.MemberService;
-import com.example.base_shopping.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SingletonTest {
+public class SingletonService {
+    private static final SingletonService instance = new SingletonService();
 
-    @Test
-    @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
-        AppConfig appConfig = new AppConfig();
+    public static SingletonService getInstance(){
+        return instance;
+    }
 
-        MemberService memberService1 = appConfig.memberService();
+    private SingletonService(){
 
-        MemberService memberService2 = appConfig.memberService();
-
-        System.out.println(memberService1);
-        System.out.println(memberService2);
-
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+    public void logic(){
+        System.out.println("싱글톤 객체 호출");
     }
 }
