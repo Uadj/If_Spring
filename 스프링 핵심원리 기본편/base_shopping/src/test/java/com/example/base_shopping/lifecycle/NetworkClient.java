4ab79be04f837baeccf232c0847d981364288592
DGW <ds257873@naver.com>
2023-10-26 22:37:47 +0900

    @PostContruct, @PreDestroy

--- 스프링 핵심원리 기본편/base_shopping/src/test/java/com/example/base_shopping/lifecycle/NetworkClient.java
+++ 스프링 핵심원리 기본편/base_shopping/src/test/java/com/example/base_shopping/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package com.example.base_shopping.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
     private String url;
     public NetworkClient(){
@@ -21,12 +24,13 @@ public class NetworkClient{
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
-
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
