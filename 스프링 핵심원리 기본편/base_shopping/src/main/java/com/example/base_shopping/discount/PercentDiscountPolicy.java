5ac63d7b9a8c075f0be0b3a2bc8a4f7af1e9e65b
DGW <ds257873@naver.com>
2023-10-26 21:29:39 +0900

    직접 Annotation 생성

--- 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/discount/PercentDiscountPolicy.java
+++ 스프링 핵심원리 기본편/base_shopping/src/main/java/com/example/base_shopping/discount/PercentDiscountPolicy.java
@@ -1,12 +1,14 @@
 package com.example.base_shopping.discount;
 
+import com.example.base_shopping.annotation.MainDiscountPolicy;
 import com.example.base_shopping.member.Grade;
 import com.example.base_shopping.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class PercentDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
