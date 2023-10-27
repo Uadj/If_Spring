7b3e9a99e728a958241ab60638b9849a87990a5b
dgw <30551889+Uadj@users.noreply.github.com>
2023-10-28 03:25:33 +0900

    로그 추적기 V1

---
+++ 스프링 핵심원리 고급편/advanced/src/test/java/com/example/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,21 @@
+package com.example.advanced.trace.hellotrace;
+
+import com.example.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+    @Test
+    void begin_end(){
+        HelloTraceV1 helloTraceV1 = new HelloTraceV1();
+        TraceStatus status = helloTraceV1.begin("hello");
+        helloTraceV1.end(status);
+    }
+    @Test
+    void begin_exception(){
+        HelloTraceV1 helloTraceV1 = new HelloTraceV1();
+        TraceStatus status = helloTraceV1.begin("hello");
+        helloTraceV1.exception(status, new IllegalStateException());
+    }
+}
