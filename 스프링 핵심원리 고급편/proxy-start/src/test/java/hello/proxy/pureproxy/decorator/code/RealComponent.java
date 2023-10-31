bc4e1aabb7b0e3217cf127e9e9e8c5f8da17c034
DGW <ds257873@naver.com>
2023-10-31 23:56:58 +0900

    데코레이터 패턴

---
+++ 스프링 핵심원리 고급편/proxy-start/src/test/java/hello/proxy/pureproxy/decorator/code/RealComponent.java
@@ -0,0 +1,14 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RealComponent implements Component{
+
+    @Override
+    public String operation() {
+        log.info("실제 컴포넌트 실행");
+        return "data";
+    }
+
+}
